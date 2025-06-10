df41e11a23f69fa6704102489b48c532f965f7d0
Sotatek-NguyenTran2 <nguyen.tran2@sotatek.com>
2025-06-10 09:08:22 +0700

    implement sign in screen

--- lib/constants/app_image_path.dart
+++ lib/constants/app_image_path.dart
@@ -3,6 +3,6 @@ class AppImagePath {
   static const String flutterLogoImg = 'assets/images/flutter_logo.png';
   static const String oclockImg = 'assets/images/oclock_img.svg';
   static const String personInPhoneImg =
-      'assets/images/person_in_phone_img.svg';
+      'assets/images/person_in_iphone_img.svg';
   static const String personIphoneImg = 'assets/images/person_iphone_img.svg';
 }
